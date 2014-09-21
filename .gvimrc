3c046246065a0a0d5b67c0a7d141fdc414c60487
YasutomoUemori <wakaba260@wakaba260.local>
2014-09-22 00:55:17 +0900

    Mac移行につきvimrc大改造

--- .gvimrc
+++ .gvimrc
@@ -1,1 +1,2 @@
 colorscheme molokai
+" set guifont=Ricty:h14
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,9 +1,5 @@
 #! /bin/bash
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.vim ~/.vim
-ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
