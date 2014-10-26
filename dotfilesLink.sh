a9e8ef6facb7162cb72cfa284b95f3ca049c3b2d
Yasutomo Uemori <moonoverwalker@gmail.com>
2014-10-26 13:49:27 +0900

    fix shell scripts

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,4 +4,5 @@ ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore
 
--- mklink.bat
+++ mklink.bat
@@ -3,4 +3,5 @@ mklink /d %HOMEPATH%"\.vim" %HOMEPATH%"\dotfiles\.vim"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.zshrc" %HOMEPATH%"\dotfiles\.zshrc"
 mklink %HOMEPATH%"\.tmux.conf" %HOMEPATH%"\dotfiles\.tmux.conf"
+mklink %HOMEPATH%"\.gitignore" %HOMEPATH%"\dotfiles\.gitignore_global"
 exit 0
