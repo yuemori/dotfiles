129342cee31cd78ee790e0e5737c35605902ac6f
yuemori <yuemori@aiming-inc.com>
2015-05-14 03:24:49 +0000

    gitignoreを統一

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -4,5 +4,5 @@ ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
 ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.gitignore_global ~/.gitignore
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
