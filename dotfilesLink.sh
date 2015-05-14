29a1ceff74caf748331f1019d0fbca0ce9b2d907
yuemori <yuemori@aiming-inc.com>
2015-05-14 03:24:23 +0000

    tigrcを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,4 +5,4 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore
-
+ln -sf ~/dotfiles/.tigrc ~/.tigrc
