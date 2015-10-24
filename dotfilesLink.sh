f3b8e23a2ca5b0d3bd5f6466f24209c9f12957dd
yuemori <yuemori@aiming-inc.com>
2015-10-24 15:47:18 +0900

    Added gitignore_global

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -5,6 +5,7 @@ ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
 ln -sf ~/dotfiles/peco ~/.peco
 
