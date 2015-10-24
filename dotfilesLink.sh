3945aa8554d523a9595c6b642d26db8fe06af0b3
yuemori <yuemori@aiming-inc.com>
2015-10-24 15:47:46 +0900

    Updated dotfilesLink to make git_tmp

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
+# files
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -sf ~/dotfiles/.vim ~/.vim
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tigrc ~/.tigrc
-ln -sf ~/dotfiles/peco ~/.peco
 
+# directories
+ln -sf ~/dotfiles/peco ~/
+ln -sf ~/dotfiles/git_tmp ~/
+ln -sf ~/dotfiles/.vim ~/
