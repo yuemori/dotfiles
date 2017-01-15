fd03387f33307caf1229c12ff96841d23f6627be
yuemori <yuemori@aiming-inc.com>
2017-01-15 19:25:15 +0900

    (・ω・)

--- setup.sh
+++ setup.sh
@@ -2,20 +2,20 @@
 
 set -eux
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap Homebrew/bundle
-brew bundle
+# /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# brew tap Homebrew/bundle
+# brew bundle install --force
 
 # files
-ln -vnsf ~/dotfiles/.vimrc ~/.vimrc
-ln -vnsf ~/dotfiles/.gvimrc ~/.gvimrc
-ln -vnsf ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -vnsf ~/dotfiles/.zshrc ~/.zshrc
-ln -vnsf ~/dotfiles/.gitignore ~/.gitignore
-ln -vnsf ~/dotfiles/.gitignore_global ~/.gitignore_global
-ln -vnsf ~/dotfiles/.tigrc ~/.tigrc
-ln -vnsf ~/dotfiles/dein.toml ~/.dein.toml
-ln -vnsf ~/dotfiles/dein_lazy.toml ~/.dein_lazy.toml
+ln -vnsf ${HOME}/dotfiles/.vimrc ~/.vimrc
+ln -vnsf ${HOME}/dotfiles/.gvimrc ~/.gvimrc
+ln -vnsf ${HOME}/dotfiles/.tmux.conf ~/.tmux.conf
+ln -vnsf ${HOME}/dotfiles/.zshrc ~/.zshrc
+ln -vnsf ${HOME}/dotfiles/.gitignore ~/.gitignore
+ln -vnsf ${HOME}/dotfiles/.gitignore_global ~/.gitignore_global
+ln -vnsf ${HOME}/dotfiles/.tigrc ~/.tigrc
+ln -vnsf ${HOME}/dotfiles/dein.toml ~/.dein.toml
+ln -vnsf ${HOME}/dotfiles/dein_lazy.toml ~/.dein_lazy.toml
 mkdir -p ${HOME}/.config/nvim
 ln -snfv ${HOME}/.vim ${HOME}/.config/nvim/
 ln -snfv ${HOME}/.vimrc ${HOME}/.config/nvim/init.vim
@@ -25,3 +25,6 @@ ln -vnsf ~/dotfiles/.peco ~/
 ln -vnsf ~/dotfiles/.git_tmp ~/
 ln -vnsf ~/dotfiles/.vim ~/
 ln -vnsf ~/dotfiles/.zsh ~/
+
+cp ${HOME}/dotfiles/localhost.homebrew-upgrade.plist ~/Library/LaunchAgents/
+launchctl load ~/Library/LaunchAgents/localhost.homebrew-upgrade.plist
