49a40381aa2de5a82d724cfce76aba7d3d5dc206
yuemori <yuemori@aiming-inc.com>
2017-07-03 00:29:32 +0900

    Fix script

--- run.sh
+++ run.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-if [ ! $(which mitamae) ];then
-  wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-darwin -O /usr/bin/mitamae
-  chmod +x /usr/bin/itamae
-fi
+case ${OSTYPE} in
+  darwin*)
+    if [ ! $(which mitamae) ];then
+      wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-darwin -O /usr/bin/mitamae
+      chmod +x /usr/bin/itamae
+    fi
+  ;;
 
-wget https://raw.githubusercontent.com/yuemori/dotfiles/master/provision.rb -O /tmp/provision.rb
+  linux*)
+    if [ -z "$(which mitamae)" ];then
+      sudo wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-linux -O /usr/local/bin/mitamae
+      sudo chmod +x /usr/local/bin/itamae
+    fi
 
-mitamae local --log-level=debug /tmp/provision.rb
+    if [ -z "$(which svn)" ];then
+      sudo apt-get update -y
+      sudo apt-get install svn
+    fi
+  ;;
+esac
+
+
+svn export --force https://github.com/yuemori/dotfiles.git/trunk/provisioning /tmp/provisioning
+mitamae local --log-level=debug /tmp/provisioning/roles/dotfile.rb
