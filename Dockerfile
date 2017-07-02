7348c3c76c71301a03306e77ec0a971a2c119b46
yuemori <yuemori@aiming-inc.com>
2017-07-03 00:55:10 +0900

    Fix dockerfile, run.sh

--- Dockerfile
+++ Dockerfile
@@ -13,14 +13,12 @@ RUN groupadd $user \
     && useradd -g $user -s /bin/bash -m $user \
     && echo "$user ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 
-RUN wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-linux -O /usr/local/bin/mitamae \
-    && chmod +x /usr/local/bin/mitamae
-
 WORKDIR /home/$user
 
 USER $user
 ENV USER=$user
 
-RUN curl https://raw.githubusercontent.com/yuemori/dotfiles/master/run.sh | bash -l
+COPY run.sh run.sh
+RUN ./run.sh
 
 CMD ["/bin/zsh"]
--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ case ${OSTYPE} in
   darwin*)
     if [ ! $(which mitamae) ];then
       wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-darwin -O /usr/bin/mitamae
-      chmod +x /usr/bin/itamae
+      chmod +x /usr/bin/mitamae
     fi
 
     echo "{ \"home\":\"/Users/$USER\",\"current_user\":\"$USER\" }" > $HOME/node.json
@@ -18,7 +18,7 @@ case ${OSTYPE} in
   linux*)
     if [ -z "$(which mitamae)" ];then
       sudo wget https://github.com/itamae-kitchen/mitamae/releases/download/v1.4.5/mitamae-x86_64-linux -O /usr/local/bin/mitamae
-      sudo chmod +x /usr/local/bin/itamae
+      sudo chmod +x /usr/local/bin/mitamae
     fi
 
     if [ -z "$(which svn)" ];then
